706902298c7f3e090c0dddec0ffe3eba972eedb2
Hong Baojin <13809735212@163.com>
2019-04-25 00:05:32 +0800

    solve seconde-menu

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+$(function () {
+  // 导航容器的宽度
+  $(".item-content").width($(".item-title").outerWidth(true));
+
+  $(".item-title").click(function(){
+    $(".item-content").css("display", "block");
+  });
+});
