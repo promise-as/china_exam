4d8cf9cdeb087be619a804c26c0681433623d274
Hong Baojin <13809735212@163.com>
2019-04-25 18:33:09 +0800

    wirte foote

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,19 @@
 $(function () {
-  // 导航容器的宽度
-  $(".item-content").width($(".item-title").outerWidth(true));
+  $(".nav .nav-item").each(function (index, ele) {
+    // 导航容器的宽度
+    $(ele).outerWidth($(ele).children(".item-title").outerWidth());
+    // 导航内容的宽度
+    $(ele).children(".item-content").outerWidth($(ele).outerWidth());
+    // 导航切换
+    $(ele).click(function () {
+      $(this).addClass("active").siblings().removeClass("active")
+    })
+  })
 
-  $(".item-title").click(function(){
-    $(".item-content").css("display", "block");
+  $(".item-content .text").mouseover(function(){
+    $(this).addClass("active").siblings().removeClass("active");
   });
+
+  // 评论 top-item 的高度
+  $(".top-item").outerHeight($(".top-item .text").outerHeight() + 46)
 });
